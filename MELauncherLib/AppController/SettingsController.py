2e9c35aeb58b0ff544ed4b501355d132b8d43374
LxHTT <lxhtt@mcsl.com.cn>
2024-01-31 13:51:25 +0800

    Fix | settings

--- MELauncherLib/AppController/SettingsController.py
+++ MELauncherLib/AppController/SettingsController.py
@@ -1,12 +1,14 @@
 from typing import Union
-
+import sys
+from os import path as osp
 from PyQt5.QtCore import QFile
 
-from qmaterialwidgets import QConfig, ConfigItem, BoolValidator
+from qmaterialwidgets import QConfig, qconfig, Theme, ConfigItem, BoolValidator
 
 
 class Config(QConfig):
     """MEFrp Launcher Configuration"""
+
     isLoggedIn = ConfigItem("User", "isLoggedIn", False, BoolValidator())
     userName = ConfigItem("User", "userName", "", "")
     userPassword = ConfigItem("User", "userToken", "", "")
@@ -23,3 +25,15 @@ def getStyleSheetFromFile(file: Union[str, QFile]):
     qss = str(f.readAll(), encoding="utf-8")
     f.close()
     return qss
+
+
+def initMELauncherConfig(loading=False):
+    qconfig.load("MEFrp-Launcher-Settings.json", cfg)
+    cfg.set(cfg.oldExecuteable, osp.basename(sys.executable))
+    if loading:
+        return
+    cfg.set(cfg.isLoggedIn, False)
+    cfg.set(cfg.userName, "")
+    cfg.set(cfg.userPassword, "")
+    cfg.set(cfg.themeColor, "#6750A4")
+    cfg.set(cfg.themeMode, Theme.AUTO)
